f9f9a7b1ad4070daa9b4e121f664f64f9f5342f4
root <root@ip-172-31-13-7.us-east-2.compute.internal>
2021-07-29 15:19:14 +0000

    push

--- clone.sh
+++ clone.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-git diff --name-only origin/master...
+git --git-dir="/dir/.git" fetch origin
+
+if git --git-dir="/dir/.git" log HEAD..origin/master --oneline
+then
+
+        git --git-dir="/dir/.git" diff --name-status ORIG_HEAD..
+
+fi
